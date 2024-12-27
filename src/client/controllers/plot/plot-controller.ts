6f9777a86baab0cb0702fd884c8d3027379824d9
RiceFarmer1 <131721290+RiceFarmer1@users.noreply.github.com>
2024-12-26 19:45:55 -0800

    little changes

--- src/client/controllers/plot/plot-controller.ts
+++ src/client/controllers/plot/plot-controller.ts
@@ -1,8 +1,43 @@
-import { Controller, OnStart } from "@flamework/core";
+import { Controller, OnStart, OnInit } from "@flamework/core";
+import { HttpService } from "@rbxts/services";
+import { Janitor } from "@rbxts/better-janitor";
+import { Players } from "@rbxts/services";
+
+const uniqueId = HttpService.GenerateGUID(false);
+
+const KEY_CODES = new ReadonlySet<Enum.UserInputType | Enum.KeyCode>([Enum.UserInputType.MouseButton1]);
 
 @Controller({})
-export class PlotController implements OnStart {
-    onStart() {
-        
-    }
+export class PlotController implements OnStart, OnInit {
+	private janitor = new Janitor<string>();
+
+	public destroy() {
+		this.janitor.destroy();
+	}
+
+	public cleanup() {
+		this.janitor.cleanup();
+	}
+
+	onInit(): void | Promise<void> {
+		function plotListener(player: Player, callback: (plot: string) => void): () => void {
+			if (player.Character) {
+				callback(uniqueId);
+			}
+
+			return () => {};
+		}
+
+		function getUnOccupiedPlot() {
+			const tycoons = new Map<number, number>();
+
+			if (tycoons.isEmpty()) {
+				return;
+			}
+
+			function getRandomPlot() {}
+		}
+	}
+
+	onStart() {}
 }
