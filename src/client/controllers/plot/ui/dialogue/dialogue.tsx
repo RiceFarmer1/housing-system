ca8892748de658bda2109bcb8b7a585917162382
Lil Baby <jermfromamerica@gmail.com>
2024-12-29 21:09:46 +0700

    c

--- src/client/controllers/plot/ui/dialogue/dialogue.tsx
+++
@@ -1,8 +0,0 @@
-import { Remotes } from "../shop-ui/remotes/remote";
-import { Players, ReplicatedStorage } from "@rbxts/services";
-import Roact from "@rbxts/roact";
-
-interface DialogueProps {
-	text: string;
-	options: { text: string; onClick: () => void }[];
-}
--- src/client/controllers/plot/ui/shop-ui/remotes/remote.ts
+++
@@ -1,6 +0,0 @@
-import { ReplicatedStorage } from "@rbxts/services";
-
-export const Remotes = {
-	ClientToServer: new Instance("RemoteEvent", ReplicatedStorage),
-	ServerToClient: new Instance("RemoteEvent", ReplicatedStorage),
-};
